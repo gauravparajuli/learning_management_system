d595a1b425a7ec509b252d28eeda7a497e4808f7
gaurav <gparajuli.1997@gmail.com>
2023-12-09 09:04:10 +0545

    implemented generic api views for teacher model

--- main/views.py
+++ main/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 from . import serializers
 from . import models
 
 # Create your views here.
-class TeacherList(APIView):
-    def get(self, request):
-        teachers = models.Teacher.objects.all()
-        serializer = serializers.TeacherSerializer(teachers, many=True)
-        return Response(serializer.data)
+# class TeacherList(APIView):
+#     def get(self, request):
+#         teachers = models.Teacher.objects.all()
+#         serializer = serializers.TeacherSerializer(teachers, many=True)
+#         return Response(serializer)
+
+class TeacherList(generics.ListCreateAPIView):
+    queryset = models.Teacher.objects.all()
+    serializer_class = serializers.TeacherSerializer
+
+class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Teacher.objects.all()
+    serializer_class = serializers.TeacherSerializer
