578f5c7fa3c3d0cad0601f7d3f554540010a9e2f
gaurav <gparajuli.1997@gmail.com>
2023-12-09 08:34:31 +0545

    created teacher, student, course, coursecategory model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    qualificaton = models.CharField(max_length=200)
+    mobile_no = models.CharField(max_length=20)
+    address = models.TextField()
+
+class CourseCategory(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'course categories'
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Course(models.Model):
+    category = models.ForeignKey(CourseCategory, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Student(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    qualificaton = models.CharField(max_length=200)
+    mobile_no = models.CharField(max_length=20)
+    address = models.TextField()
+    intrested_categories = models.TextField()
