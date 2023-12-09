803afd6e0644086e9de5e27e3c34880668e7cdb7
gaurav <gparajuli.1997@gmail.com>
2023-12-09 08:55:40 +0545

    implemented teacherlist apiview

--- main/models.py
+++ main/models.py
@@ -5,7 +5,7 @@ class Teacher(models.Model):
     full_name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
-    qualificaton = models.CharField(max_length=200)
+    qualification = models.CharField(max_length=200)
     mobile_no = models.CharField(max_length=20)
     address = models.TextField()
 
