eea8f063f46f16c17608b582bfde8baf28ccd2da
gaurav <gparajuli.1997@gmail.com>
2023-12-09 16:17:12 +0545

    added permission(authentication)

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework import permissions
 from . import serializers
 from . import models
 
@@ -15,7 +16,9 @@ from . import models
 class TeacherList(generics.ListCreateAPIView):
     queryset = models.Teacher.objects.all()
     serializer_class = serializers.TeacherSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Teacher.objects.all()
     serializer_class = serializers.TeacherSerializer
+    permission_classes = [permissions.IsAuthenticated]
